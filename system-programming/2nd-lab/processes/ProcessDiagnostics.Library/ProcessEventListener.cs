3e26954f3a6e15d29d8e4203966a2e34dbf36871
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2022-11-11 09:16:02 +0600

    1st version

--- system-programming/2nd-lab/processes/ProcessDiagnostics.Library/ProcessEventListener.cs
+++ system-programming/2nd-lab/processes/ProcessDiagnostics.Library/ProcessEventListener.cs
@@ -8,7 +8,9 @@ namespace ProcessDiagnostics.Library
     {
         private static readonly IConfiguration _config = new ConfigurationBuilder().AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\ProcessDiagnostics.Library\config.json")).Build();
         private static readonly string? _logFilePath = _config["processLogPath"];
-
+        private readonly XmlWriterSettings _settings;
+        private readonly XmlSerializerNamespaces _xmlns;
+        private readonly object _lock = new();
         private static string LogFilePath
         {
             get
@@ -23,18 +25,18 @@ namespace ProcessDiagnostics.Library
         public ProcessEventListener()
         {
             File.Create(LogFilePath).Dispose();
+            _settings = new() { Indent = true, OmitXmlDeclaration = true };
+            _xmlns = new(new[] { XmlQualifiedName.Empty });
         }
 
         public void Log(object? _, ProcessEventInfo? processInfo)
         {
-            XmlWriterSettings settings = new() { Indent = true, OmitXmlDeclaration = true };
-            XmlSerializerNamespaces xmlns = new(new[] { XmlQualifiedName.Empty });
-            lock (settings)
+            lock (_lock)
             {
                 using StreamWriter streamWriter = new(LogFilePath, true);
-                using XmlWriter writer = XmlWriter.Create(streamWriter, settings);
+                using XmlWriter writer = XmlWriter.Create(streamWriter, _settings);
                 XmlSerializer xmlSerializer = new(typeof(ProcessEventInfo));
-                xmlSerializer.Serialize(writer, processInfo, xmlns);
+                xmlSerializer.Serialize(writer, processInfo, _xmlns);
                 streamWriter.Write(Environment.NewLine);
             }
         }
--- system-programming/2nd-lab/processes/ProcessDiagnostics/Program.cs
+++ system-programming/2nd-lab/processes/ProcessDiagnostics/Program.cs
@@ -9,6 +9,259 @@ namespace ProcessDiagnostics
     public static class Program
     {
         private static readonly IConfiguration _config = new ConfigurationBuilder().AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\config.json")).Build();
+        private static readonly int? _warningNotificationDelay = _config.GetValue<int?>("warningNotificationDelay");
+        private static readonly int? _notificationDelay = _config.GetValue<int?>("notificationDelay");
+        private static readonly string? _limitsFilePath = _config.GetValue<string?>("processLimitPath");
+
+        public static event EventHandler<ProcessEventInfo>? ProcessEvent;
+
+        private static int WarningNotificationDelay
+        {
+            get
+            {
+                if (_warningNotificationDelay is null)
+                    throw new InvalidOperationException($"Config file hasn't been initialized.");
+
+                return _warningNotificationDelay.Value;
+            }
+        }
+        private static int NotificationDelay
+        {
+            get
+            {
+                if (_notificationDelay is null)
+                    throw new InvalidOperationException($"Config file hasn't been initialized.");
+
+                return _notificationDelay.Value;
+            }
+        }
+        private static string LimitsFilePath
+        {
+            get
+            {
+                if (_limitsFilePath is null)
+                    throw new InvalidOperationException($"Config file hasn't been initialized.");
+
+                return _limitsFilePath;
+            }
+        }
+
+        static async Task<int> Main(string[] args)
+        {
+            string processName = args[0];
+            if (processName is null)
+            {
+                Console.WriteLine("Please, type in the name of the process you wish to inspect after the program's name.");
+                return 0;
+            }
+
+            XmlSerializer xmlSerializer = new(typeof(ProcessLimit));
+            ProcessLimit? processLimit;
+            using (FileStream processLimitFileStream = new(LimitsFilePath, FileMode.Open))
+            {
+                processLimit = (ProcessLimit?)xmlSerializer.Deserialize(processLimitFileStream);
+                if (processLimit is null)
+                    return -1;
+            }
+
+            double fraction = .85;
+            ProcessLimit dangerousThreshold = new()
+            {
+                MemoryUsageLimit = (long)(processLimit.MemoryUsageLimit * fraction),
+                ProcessorTimeLimit = (int)(processLimit.ProcessorTimeLimit * fraction),
+                ThreadCountLimit = (int)Math.Floor(processLimit.ThreadCountLimit * fraction),
+                HandleCountLimit = (int)Math.Floor(processLimit.HandleCountLimit * fraction)
+            };
+
+            ProcessEventListener listener = new();
+            ProcessEvent += listener.Log;
+            using Process process = Process.Start(processName);
+
+            ProcessMessageBuilder message = new(process);
+            ProcessEvent.Invoke(null, new ProcessEventInfo(process) { Type = 0, Message = "Started" });
+            Task[] warningTasks = new Task[4];
+            for (int i = 0; i < warningTasks.Length; i++)
+                warningTasks[i] = Task.CompletedTask;
+
+            using CancellationTokenSource cancellationTokenSource = new();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            Task logging = LogState(process, message, cancellationToken);
+
+            while (!process.HasExited)
+            {
+                if (process.WorkingSet64 >= processLimit.MemoryUsageLimit)
+                {
+                    message.MemorySeverity = Severity.Error;
+                    LogError(process, message);
+                    process.Kill();
+                    break;
+                }
+                else if (process.WorkingSet64 >= dangerousThreshold.MemoryUsageLimit)
+                {
+                    if (warningTasks[0].IsCompleted)
+                    {
+                        message.MemorySeverity = Severity.Warning;
+                        warningTasks[0] = Task.Run(() => LogWarning(process, message));
+                    }
+                }
+                else
+                    message.MemorySeverity = Severity.None;
+
+
+                if (process.TotalProcessorTime.Milliseconds >= processLimit.ProcessorTimeLimit)
+                {
+                    message.ProcessorTimeSeverity = Severity.Error;
+                    LogError(process, message);
+                    process.Kill();
+                    break;
+                }
+                else if (process.TotalProcessorTime.Milliseconds >= dangerousThreshold.ProcessorTimeLimit)
+                {
+                    if (warningTasks[1].IsCompleted)
+                    {
+                        message.ProcessorTimeSeverity = Severity.Warning;
+                        warningTasks[1] = Task.Run(() => LogWarning(process, message));
+                    }
+                }
+                else
+                    message.ProcessorTimeSeverity = Severity.None;
+
+
+                if (process.Threads.Count >= processLimit.ThreadCountLimit)
+                {
+                    message.ThreadCountSeverity = Severity.Error;
+                    LogError(process, message);
+                    process.Kill();
+                    break;
+                }
+                else if (process.Threads.Count >= dangerousThreshold.ThreadCountLimit)
+                {
+                    if (warningTasks[2].IsCompleted)
+                    {
+                        message.ThreadCountSeverity = Severity.Warning;
+                        warningTasks[2] = Task.Run(() => LogWarning(process, message));
+                    }
+                }
+                else
+                    message.ThreadCountSeverity = Severity.None;
+
+                if (process.HandleCount >= processLimit.HandleCountLimit)
+                {
+                    message.HandleCountSeverity = Severity.Error;
+                    LogError(process, message);
+                    process.Kill();
+                    break;
+                }
+                else if (process.HandleCount >= dangerousThreshold.HandleCountLimit)
+                {
+                    if (warningTasks[3].IsCompleted)
+                    {
+                        message.HandleCountSeverity = Severity.Warning;
+                        warningTasks[3] = Task.Run(() => LogWarning(process, message));
+                    }
+                }
+                else
+                    message.HandleCountSeverity = Severity.None;
+            }
+
+            cancellationTokenSource.Cancel();
+            await logging;
+            return 0;
+        }
+
+        /// <summary>
+        /// Logs a notification message every specified(in the config file) seconds. Runs along with the state loop.
+        /// </summary>
+        /// <param name="process">Process we wish to track.</param>
+        /// <param name="message">Message to be displayed in the log.</param>
+        /// <param name="token">Cancellation token in case we'd like to stop.</param>
+        /// <returns>Awaitable task representing the background work.</returns>
+        private static async Task LogState(Process process, ProcessMessageBuilder message, CancellationToken token)
+        {
+            while (true)
+            {
+                try
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(NotificationDelay), token).ContinueWith((_) =>
+                    {
+                        if (token.IsCancellationRequested)
+                            token.ThrowIfCancellationRequested();
+
+                        ProcessEvent?.Invoke(null, new ProcessEventInfo(process)
+                        {
+                            Type = ProcessEventType.Notification,
+                            Message = message.ToString()
+                        });
+                    }, token);
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Logs a warning message. It's made asynchronous because we'd like to log the message in specific time
+        /// intervals once we've hit the warning state, while also keep the state loop checking the process for errors.
+        /// </summary>
+        /// <param name="process">Process we're currently tracking.</param>
+        /// <param name="message">Warning message to be displayed in the log.</param>
+        /// <returns>Awaitable task representing the background work.</returns>
+        private static async Task LogWarning(Process process, ProcessMessageBuilder message)
+        {
+            ProcessEvent?.Invoke(null, new ProcessEventInfo(process)
+            {
+                Type = ProcessEventType.Warning,
+                Message = message.ToString()
+            });
+            await Task.Delay(TimeSpan.FromSeconds(WarningNotificationDelay));
+        }
+
+        /// <summary>
+        /// Logs an error message.
+        /// </summary>
+        /// <param name="process">Process we're currently tracking.</param>
+        /// <param name="message">Error message to be displayed in the log.</param>
+        private static void LogError(Process process, ProcessMessageBuilder message)
+        {
+            ProcessEvent?.Invoke(null, new ProcessEventInfo(process)
+            {
+                Type = ProcessEventType.Error,
+                Message = message.ToString()
+            });
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+ using Microsoft.Extensions.Configuration;
+using ProcessDiagnostics.Library;
+using System.Diagnostics;
+using System.Xml.Serialization;
+using System.Timers;
+
+namespace ProcessDiagnostics
+{
+
+    public static class Program
+    {
+        private static readonly IConfiguration _config = new ConfigurationBuilder().AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\config.json")).Build();
         public static event EventHandler<ProcessEventInfo>? ProcessEvent;
 
         static async Task<int> Main(string[] args)
@@ -25,21 +278,21 @@ namespace ProcessDiagnostics
                 return -1;
 
             XmlSerializer xmlSerializer = new(typeof(ProcessLimit));
-            ProcessLimit? processLimits;
+            ProcessLimit? processLimit;
             using (FileStream processLimitFileStream = new(processLimitFilePath, FileMode.Open))
             {
-                processLimits = (ProcessLimit?)xmlSerializer.Deserialize(processLimitFileStream);
-                if (processLimits is null)
+                processLimit = (ProcessLimit?)xmlSerializer.Deserialize(processLimitFileStream);
+                if (processLimit is null)
                     return -1;
             }
 
             double fraction = .85;
             ProcessLimit dangerousThreshold = new()
             {
-                MemoryUsageLimit = (long)(processLimits.MemoryUsageLimit * fraction),
-                ProcessorTimeLimit = (int)(processLimits.ProcessorTimeLimit * fraction),
-                ThreadCountLimit = (int)Math.Floor(processLimits.ThreadCountLimit * fraction),
-                HandleCountLimit = (int)Math.Floor(processLimits.HandleCountLimit * fraction)
+                MemoryUsageLimit = (long)(processLimit.MemoryUsageLimit * fraction),
+                ProcessorTimeLimit = (int)(processLimit.ProcessorTimeLimit * fraction),
+                ThreadCountLimit = (int)Math.Floor(processLimit.ThreadCountLimit * fraction),
+                HandleCountLimit = (int)Math.Floor(processLimit.HandleCountLimit * fraction)
             };
             ProcessEventListener listener = new();
             ProcessEvent += listener.Log;
@@ -50,9 +303,11 @@ namespace ProcessDiagnostics
             using CancellationTokenSource cancellationTokenSource = new();
             CancellationToken cancellationToken = cancellationTokenSource.Token;
             Task logging = LogState(process, message, cancellationToken);
+            System.Timers.Timer timer = new(1000) { AutoReset = true };
+            timer.Start();
             while (!process.HasExited)
             {
-                if (process.WorkingSet64 >= processLimits.MemoryUsageLimit)
+                if (process.WorkingSet64 >= processLimit.MemoryUsageLimit)
                 {
                     message.MemorySeverity = Severity.Error;
 
@@ -78,7 +333,7 @@ namespace ProcessDiagnostics
                     message.MemorySeverity = Severity.None;
 
 
-                if (process.TotalProcessorTime.Milliseconds >= processLimits.ProcessorTimeLimit)
+                if (process.TotalProcessorTime.Milliseconds >= processLimit.ProcessorTimeLimit)
                 {
                     message.ProcessorTimeSeverity = Severity.Error;
 
@@ -104,7 +359,7 @@ namespace ProcessDiagnostics
                     message.ProcessorTimeSeverity = Severity.None;
 
 
-                if (process.Threads.Count >= processLimits.ThreadCountLimit)
+                if (process.Threads.Count >= processLimit.ThreadCountLimit)
                 {
                     message.ThreadCountSeverity = Severity.Error;
 
@@ -129,7 +384,7 @@ namespace ProcessDiagnostics
                 else
                     message.ThreadCountSeverity = Severity.None;
 
-                if (process.HandleCount >= processLimits.HandleCountLimit)
+                if (process.HandleCount >= processLimit.HandleCountLimit)
                 {
                     message.HandleCountSeverity = Severity.Error;
 
@@ -185,4 +440,4 @@ namespace ProcessDiagnostics
             }
         }
     }
-}
+}*/
