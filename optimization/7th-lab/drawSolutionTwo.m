369ad6771bed774ae3ed1198408a4eedaa056fd4
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2022-12-09 15:38:22 +0600

    the last lab finished.

--- optimization/7th-lab/drawSolutionTwo.m
+++ optimization/7th-lab/drawSolutionTwo.m
@@ -1,12 +1,12 @@
-function [] = drawSolution(f, fun, u1ord, u2ord)
+function [] = drawSolutionTwo(f, fun, u1ord, u2ord)
     ca = [-1 -1; -7 8; 3 2];
-    cb = [-5; 0; 18];
+    cb = [-6; 0; 18];
     clb = [0; 0];
     cub = [4; 6];
     disp('Решение проблемы будет иметь вид:')
-    u = linprog(f, ca, cb, [], [], clb, cub)
+    u = linprog(f, ca, cb, [], [], clb, cub);
     disp('Оптимальное значение критерия:')
-    j0 = f*u
+    j0 = f'*u
     u01 = [1 0] * u;
     u02 = [0 1] * u;
     [u1, u2] = meshgrid(u1ord, u2ord);
@@ -18,13 +18,13 @@ function [] = drawSolution(f, fun, u1ord, u2ord)
     title('Solution of problem');
     hold on;
     
-    % 1st constraint
-    u2ord = 5 - u1ord;
+    % 1st constraint. Above
+    u2ord = 6 - u1ord;
     plot(u1ord, u2ord, 'y');
-    % 2nd constraint
+    % 2nd constraint. Below
     u2ord = (7 ./ 8) * u1ord;
     plot(u1ord, u2ord, 'b');
-    % 3rd constraint 
+    % 3rd constraint. Below
     u2ord = 9 - ((3 ./ 2) * u1ord);
     plot(u1ord, u2ord, 'g'); 
     % 4th constraint
--- optimization/7th-lab/mainn.m
+++ optimization/7th-lab/mainn.m
@@ -1,17 +1,97 @@
-f = [1 0];
+f = [-1 0]';
+% maximize u1
 fun = @(u1, u2) u1;
 u1ord = 0: 1: 10;
 u2ord = 0: 1: 10;
-drawSolution(f, fun, xord, uord);
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
 
-f = [-1 0];
-fun = @(u1, u2) -u1;
-drawSolution(f, fun, xord, uord);
-
-f = [0 1];
+% minimize u1
+f = [1 0]';
 fun = @(u1, u2) u1;
-drawSolution(f, fun, xord, uord);
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
 
-f = [0 -1];
-fun = @(u1, u2) -u1;
-drawSolution(f, fun, xord, uord);
+% maximize u2
+f = [0 -1]';
+fun = @(u1, u2) u1;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% minimize u2
+f = [0 1]';
+fun = @(u1, u2) u1;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% maximize u1 + u2
+f = [-1 -1]';
+fun = @(u1, u2) u1 + u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% minimize u1 + u2
+f = [1 1]';
+fun = @(u1, u2) u1 + u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% maximize -3*u1 + 5*u2
+f = [3 -5]';
+fun = @(u1, u2) -3.*u1 + 5.*u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% minimize 3*u1 - 5*u2
+f = [3 -5]';
+fun = @(u1, u2) 3.*u1 - 5.*u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% maximize -2*u1 - u2
+f = [2 1]';
+fun = @(u1, u2) -2.*u1 - u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% maximize -u1 + 2*u2
+f = [1 -2]';
+fun = @(u1, u2) -u1 + 2.*u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% minimize u1 - 2*u2
+f = [1 -2]';
+fun = @(u1, u2) u1 - 2.*u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
+
+% minimize 2*u1 + u2
+f = [2 1]';
+fun = @(u1, u2) 2.*u1 + u2;
+drawSolutionOne(f, fun, u1ord, u2ord);
+drawSolutionTwo(f, fun, u1ord, u2ord);
+drawSolutionThree(f, fun, u1ord, u2ord);
+drawSolutionFour(f, fun, u1ord, u2ord);
