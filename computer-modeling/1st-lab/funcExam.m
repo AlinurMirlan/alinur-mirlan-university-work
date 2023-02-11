90d834aa6f211d0c796b7f6d736cdac42b17f239
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2023-02-11 22:25:20 +0600

    1st lab done.

---
+++ computer-modeling/1st-lab/funcExam.m
@@ -0,0 +1,11 @@
+function [] = funcExam(func, manualFunc, t0, tf, y0)
+    [t, y] = ode45(func, [t0 tf], y0);
+    plot(t, y)
+    hold on
+    plot(t, manualFunc(t), '--o');
+    xlabel("Time t")
+    ylabel("Solution y")
+    hold on
+    pause
+end
+
--- computer-modeling/1st-lab/main.m
+++ computer-modeling/1st-lab/main.m
@@ -2,17 +2,18 @@ clc;
 t0 = 1;
 tf = 2;
 y0 = [8 10];
-[t, y] = ode45('vdpOne', [t0 tf], y0);
-
-plot(t, y)
-xlabel("Time t")
-ylabel("Solution y")
 A = [1 log(5./4); 0 -9./40];
 B = [8; 10];
 T = linsolve(A, B);
-pause
+func = @(t) T(1) + T(2).*log(abs((9.*t + 1)./(9.*t - 1)));
+funcExam('vdpOne', func, t0, tf, y0);
 
 t0 = 0;
 y0 = [2 -4 5];
-[t, y] = ode45('vdpTwo', [t0 tf], y0);
-plot(t, y)
+A = [1 0 0; -5 1 0; 75 -30 6];
+B = [2; -4; 5];
+T = linsolve(A, B);
+func = @(t) (T(1) + T(2).*t + T(3).*(t.^2) + (t.^2)./6).*exp(-5.*t);
+funcExam('vdpTwo', func, t0, tf, y0);
+
+
