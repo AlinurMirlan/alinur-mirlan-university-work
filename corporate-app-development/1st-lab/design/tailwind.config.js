1ca195f6d2d9a28a006b1bde2417bb8c5b524cf9
Alinur Mirlan <a18nur.m1r1an@gmail.com>
2023-02-21 07:56:19 +0600

    Some design changes.

---
+++ corporate-app-development/1st-lab/design/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: '2em'
+    },
+    extend: {
+    }
+  },
+  plugins: [],
+}
